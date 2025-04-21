d945b779e5697c394d0cba4a19e68b09c5ec576b
Omar sabry <omarsabry@gmail.com>
2025-04-21 07:59:19 +0200

    update main.ts to handle errors

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,33 @@
+import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js'
 import { Sequelize } from 'sequelize-typescript'
 import { NestFactory, Reflector } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common'
 import { GeneralResponseInterceptor } from './common/interceptor/generalResponse.interceptor'
-import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.js'
+import {
+  BadRequestException,
+  ClassSerializerInterceptor,
+  ValidationPipe,
+} from '@nestjs/common'
 
 async function bootstrap () {
-  const app = await NestFactory.create(AppModule)
-  app.enableCors()
-  app.useGlobalPipes(new ValidationPipe())
-  app.useGlobalInterceptors(new GeneralResponseInterceptor())
-  app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 2 }))
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
+  try {
+    const app = await NestFactory.create(AppModule)
+    app.enableCors()
+    app.useGlobalPipes(new ValidationPipe())
+    app.useGlobalInterceptors(new GeneralResponseInterceptor())
+    app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 1 }))
+    app.useGlobalInterceptors(
+      new ClassSerializerInterceptor(app.get(Reflector)),
+    )
 
-  const sequelize = app.get(Sequelize)
-  await sequelize.sync({ alter: true })
+    const sequelize = app.get(Sequelize)
+    await sequelize.sync({ alter: true })
 
-  await app.listen(process.env.PORT)
+    await app.listen(process.env.PORT || 3000)
+  } catch (error) {
+    console.log(error)
+    throw new BadRequestException(error)
+  }
 }
 
 bootstrap()
